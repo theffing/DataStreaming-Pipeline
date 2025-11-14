0af5d1153b3da3780950fa9060cf99c07618eb8c
Taylor Heffington <heffingtontaylor@gmail.com>
2025-11-14 15:52:57 -0800

    Updating dags/Kafka_stream.py to process data from randomuser.me/api

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -2,17 +2,29 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
+# https://youtu.be/GqAcTrqKcrY?si=7Y5gIB91J3XlSXyG
+
 default_args = {
     'owner': 'airscholar',
     'start_date': datetime(2025, 11, 14, 3, 00)
 }
 
-def stream_data():
+def get_data():
     import json
     import requests
     
     res = requests.get("https://randomuser.me/api/")
-    print(res.json())
+    res = res.json()
+    res = res['results'][0]
+
+    return res
+
+def stream_data():
+    import json
+
+def format_data(res):
+    data = {}
+
 
 with DAG('user_automation', 
     default_args=default_args,
@@ -23,5 +35,3 @@ with DAG('user_automation',
         task_id='stream_data_from_api',
         python_callable=stream_data
     )
-
-stream_data()
